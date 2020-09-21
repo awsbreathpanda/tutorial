02bfcebace893d14c1955ddf3c7c7faa5fdaebfc
awsbreathpanda <awsbreathpanda@gmail.com>
2020-09-21 11:03:58 +0800

    feature: Update

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from snippets.views import SnippetListView, SnippetDetailView
+from snippets.views import SnippetListView, SnippetDetailView, UserDetailView, UserListView
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('snippets/', SnippetListView.as_view()),
     path('snippets/<int:pk>/', SnippetDetailView.as_view()),
+    path('users/', UserListView.as_view()),
+    path('users/<int:pk>', UserDetailView.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- snippets/views.py
+++ snippets/views.py
@@ -1,55 +1,31 @@
-from django.http.response import Http404
-from rest_framework import status
-from rest_framework.response import Response
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
 from snippets.models import Snippet
-from rest_framework.views import APIView
+from rest_framework import generics
+from django.contrib.auth.models import User
 
 
-# snippets.json/
-# snippets.api/
-class SnippetListView(APIView):
-    """List all snippets or create a new snippet.
-    """
-    def get(self, request, format=None):
-        snippets = Snippet.objects.all()
-        serializer = SnippetSerializer(snippets, many=True)
+# snippets/
+class SnippetListView(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
 
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = SnippetSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.users)
 
 
-# snippets/id.json/
-# snippets/id.api/
-class SnippetDetailView(APIView):
-    """Retrieve, update or delete a snippet.
-    """
-    def get_object(self, pk):
-        try:
-            return Snippet.objects.get(pk=pk)
-        except Snippet.DoesNotExist:
-            raise Http404
+# snippets/id/
+class SnippetDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
 
-    def get(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = SnippetSerializer(snippet)
-        return Response(serializer.data)
 
-    def put(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = SnippetSerializer(snippet, request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# users/
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
-    def delete(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        snippet.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+
+# users/id
+class UserDetailView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
