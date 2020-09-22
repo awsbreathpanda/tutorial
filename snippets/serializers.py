15569cd865f50839df19e47ed78e1d4792710c14
awsbreathpanda <awsbreathpanda@gmail.com>
2020-09-22 10:09:01 +0800

    feature: Update

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -2,7 +2,10 @@ from rest_framework import serializers
 from snippets.models import Snippet
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    # model中的外键必须在serializer中这样处理一下
+    owner = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
 
         model = Snippet
--- snippets/urls.py
+++ snippets/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from snippets.views import SnippetListView, SnippetDetailView
+from snippets.views import SnippetListView, SnippetDetailView, api_root, SnippetHighlight
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('snippets/', SnippetListView.as_view()),
-    path('snippets/<int:pk>/', SnippetDetailView.as_view()),
+    path('snippets/', SnippetListView.as_view(), name='snippet-list'),
+    path('snippets/<int:pk>/',
+         SnippetDetailView.as_view(),
+         name='snippet-detail'),
+    path('snippets/<int:pk>/highlight/',
+         SnippetHighlight.as_view(),
+         name='snippet-highlight'),
+    path('', api_root),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
