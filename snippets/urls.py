2a9702369d49692383de3481e217d4c068bece71
awsbreathpanda <awsbreathpanda@gmail.com>
2020-09-25 14:19:09 +0800

    feature: Update to ViewSet

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,12 +1,5 @@
-from snippets.views import SnippetDetailView, SnippetListView
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
+from snippets import views
 
-urlpatterns = [
-    path('snippets/', SnippetListView.as_view(), name='snippet-list'),
-    path('snippets/<int:pk>/',
-         SnippetDetailView.as_view(),
-         name='snippet-detail'),
-]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
--- snippets/views.py
+++ snippets/views.py
@@ -1,18 +1,18 @@
-from snippets.serializers import SnippetSerializer
-from snippets.models import Snippet
-from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from rest_framework import permissions
+from rest_framework import viewsets
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
 
 
-# snippets/
-class SnippetListView(generics.ListCreateAPIView):
+class SnippetViewSet(viewsets.ModelViewSet):
+
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-
-# snippets/id/
-class SnippetDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    @action(detail=True)
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
